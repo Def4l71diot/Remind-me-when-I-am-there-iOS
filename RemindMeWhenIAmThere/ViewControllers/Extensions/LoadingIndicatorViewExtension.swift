4367c65783d5a1b7a2d8e42b9ca261074f5c6f42
Def4l71diot <lubeto3099@gmail.com>
2017-03-27 01:18:07 +0300

    Project structure updated

---
+++ RemindMeWhenIAmThere/ViewControllers/Extensions/LoadingIndicatorViewExtension.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingIndicatorViewExtension.swift
+//  RemindMeWhenIAmThere
+//
+//  Created by Lyubomir on 3/27/17.
+//  Copyright © 2017 Vinkelton. All rights reserved.
+//
+
+import UIKit
+
+class LoadingIndicatorViewExtension: NSObject {
+
+}
