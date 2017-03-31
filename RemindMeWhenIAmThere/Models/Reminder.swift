ea25043d37fa5581b2fa27f9bdb6ff2c62df28a3
Def4l71diot <lubeto3099@gmail.com>
2017-03-31 15:19:14 +0300

    Added Reminder entity to CoreData

---
+++ RemindMeWhenIAmThere/Models/Reminder.swift
@@ -0,0 +1,13 @@
+//
+//  Reminder.swift
+//  RemindMeWhenIAmThere
+//
+//  Created by Lyubomir on 3/31/17.
+//  Copyright © 2017 Vinkelton. All rights reserved.
+//
+
+import Foundation
+
+class Reminder: ReminderEntity {
+    
+}
